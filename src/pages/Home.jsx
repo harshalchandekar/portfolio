4840e55676e01a73fd39872476cb027ca8f4b433
Harshal Chandekar <harshalchandekar2204@gmail.com>
2025-08-10 00:18:02 +0530

    implement hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import HeroSection from "../components/HeroSection";
 import NavBar from "../components/NavBar";
 import StarBackground from "../components/StarBackground";
 import ThemeToggle from "../components/ThemeToggle";
@@ -12,6 +13,8 @@ const Home = () => {
         {/* // NavBar */}
             <NavBar />
         {/* Main Content */}
+
+            <HeroSection />
 
         {/* Footer */}
         </div>
